079578573a3617a448995575eb080c1b16f63e9e
z3sb <oslvuao@gmail.com>
2022-10-10 12:45:23 +0300

    create header

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react'
+import Header from '../components/Header/Header';
+
+const Home = () => {
+  return (
+    <Fragment>
+      <Header />
+    </Fragment>
+  )
+}
+export default Home;
