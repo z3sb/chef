3f9c4e563daef6fb9358a1d25679bfc5c78d7b2c
z3sb <oslvuao@gmail.com>
2022-10-10 14:27:30 +0300

    Design Hero Slider and Pupular Menu Food Sections

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React, { Fragment } from 'react'
 import Header from '../components/Header/Header';
+import HeroSlider from '../components/hero-slider/HeroSlider';
+import PopularMenu from '../components/popular-menu/PopularMenu';
 
 const Home = () => {
   return (
     <Fragment>
       <Header />
+      <HeroSlider />
+      <PopularMenu />
     </Fragment>
   )
 }
